c795f9f636436e7d3f51019d51ada1b915563df1
Pratham Rajput <prathamrajput709@gmail.com>
2025-07-13 20:31:09 +0530

    Syntax fix

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -3,15 +3,18 @@ import pandas as pd
 from src.exception import CustomException
 from src.logger import logging
 from src.utils import load_object
-
+import os
 class PredictPipeline:
     def __init__(self):
         pass
     
     def predict(self,feature):
         try:
-            model_path='artifacts\model.pkl'
-            preprocessor_path='artifacts/preprocessor.pkl'
+           
+
+            model_path = os.path.join("artifacts", "model.pkl")
+            preprocessor_path = os.path.join("artifacts", "preprocessor.pkl")
+
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
             data_scaled=preprocessor.transform(feature)
