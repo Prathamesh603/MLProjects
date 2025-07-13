aef58d27cbd6bce383291bd8d46cb000d5fd2ec1
Pratham Rajput <prathamrajput709@gmail.com>
2025-07-13 20:40:16 +0530

    Added error print in application

--- application.py
+++ application.py
@@ -19,22 +19,28 @@ def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
     else:
-        data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+        try:
+            data=CustomData(
+                gender=request.form.get('gender'),
+                race_ethnicity=request.form.get('ethnicity'),
+                parental_level_of_education=request.form.get('parental_level_of_education'),
+                lunch=request.form.get('lunch'),
+                test_preparation_course=request.form.get('test_preparation_course'),
+                reading_score=float(request.form.get('writing_score')),
+                writing_score=float(request.form.get('reading_score'))
 
-        )
-        pred_df=data.get_data_as_data_frame()
-        print(pred_df)
+            )
+            pred_df=data.get_data_as_data_frame()
+            print(pred_df)
 
-        predict_pipeline=PredictPipeline()
-        results = predict_pipeline.predict(pred_df)
-        return render_template('home.html',results=results[0])
+            predict_pipeline=PredictPipeline()
+            results = predict_pipeline.predict(pred_df)
+            return render_template('home.html',results=results[0])
+        except Exception as e:
+            import traceback
+            print("❌ Error during prediction:", e)
+            traceback.print_exc()
+            return f"<h2 style='color:red;'>Prediction Failed: {e}</h2>", 500
     
 # if __name__=="__main__":
 #     app.run(host="0.0.0.0")
